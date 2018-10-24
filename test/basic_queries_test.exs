671704748c8667bf3b6e9d08aec6f2a4b9afb217
Christoph Klocker <christoph@vedanova.com>
2018-10-24 21:48:34 +0200

    query user test

--- test/basic_queries_test.exs
+++ test/basic_queries_test.exs
@@ -1,12 +1,22 @@
 defmodule BasicQueriesTest do
   use ExUnit.Case
-  doctest BasicQueries
-  alias BasicQueries.User
   import BasicQueries.Repo
+  import Ecto.Query, only: [from: 2]
+
+  alias BasicQueries.User
   alias BasicQueries.Repo
+
+  setup do
+    Repo.delete_all(User)
+    :ok
+  end
 
   def user_attributes do
     %{username: "max", email: "max@headroom.com"}
+  end
+
+  def username do
+    Map.get(user_attributes(), :username)
   end
 
   def create_user do
@@ -17,7 +27,21 @@ defmodule BasicQueriesTest do
   end
 
   test "create user" do
-    user = create_user
-    assert user.username == Map.get(user_attributes(), :username)
+    user = create_user()
+    assert user.username == username()
+  end
+
+  test "query user" do
+    u = create_user()
+
+    query =
+      from(
+        u in User,
+        where: u.username == "max",
+        select: u.username
+      )
+
+    name = Repo.one(query)
+    name == username()
   end
 end
